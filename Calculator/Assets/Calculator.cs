e521bf2e557249ed611c5a5eb4e709bcce52bc83
Max Ockzle <maxocklze@gmail.com>
2017-04-24 17:15:12 +0300

    initial commit

---
+++ Calculator/Assets/Calculator.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour {
+
+    [SerializeField]
+    Text InputField;
+
+    string InputString;
+    decimal[] number = new decimal[2];
+    string OperatorSymbol;
+    int i = 0;
+    decimal result;
+    bool DisplayResult = false;
+
+    public void ButtonPressed()
+    {
+
+        if (DisplayResult == true)
+        {
+            InputField.text = "";
+            DisplayResult = false;
+            InputString = "";
+        }
+
+        string buttonvalue = EventSystem.current.currentSelectedGameObject.name;
+        InputString += buttonvalue;
+
+        int arg;
+        if (int.TryParse(buttonvalue,out arg))
+        {
+            if (i > 1) i = 0;
+            number[i] = arg;
+            i = i + 1;
+        }
+        else
+        {
+            switch (buttonvalue)
+            {
+                case "+":
+                    OperatorSymbol = buttonvalue;
+                    break;
+
+                case "-":
+                    OperatorSymbol = buttonvalue;
+                    break;
+                case "*":
+                    OperatorSymbol = buttonvalue;
+                    break;
+                case "/":
+                    OperatorSymbol = buttonvalue;
+                    break;
+
+                case "C":
+                    InputField.text = "";
+                    InputString = "";
+                    break;
+
+                case "=":
+                    switch (OperatorSymbol)
+                    {
+                        case "+":
+                            result = number[0] + number[1];
+                            break;
+                        case "-":
+                            result = number[0] - number[1];
+                            break;
+                        case "*":
+                            result = number[0] * number[1];
+                            break;
+                        case "/":
+                            result = number[0] / number[1];
+                            break;
+                    }
+
+                    DisplayResult = true;
+                    InputString = result.ToString();
+                    number = new decimal[2];
+                    break;
+
+            }
+        }
+
+        InputField.text = InputString;
+
+    }
+}
